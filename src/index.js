030caebd2749c31a15c8ee888410ffafa9527a55
Anurag Sharma <2k23cs.2313830@gmail.com>
2024-11-27 19:34:26 +0530

    connect to mongodb

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// require('dotenv').config({path:'./en=nv'})
+import dotenv from "dotenv"
+// import mongoose from "mongoose"
+// import {DB_NAME} from "./constants.js"
+import connectdb from "./db/index2.js"
+// FIRST APPROACH
+// import express from "express"
+//  const app = express()
+//  (async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("not able to talk to database",error)
+//             throw error
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`app is listening on port ${process.env.PORT}`)
+//         })
+
+//     }
+//     catch(error){
+//         console.log("error",error)
+//         throw error
+//     }
+
+//  })()
+
+dotenv.config({
+    
+    path:'./env'
+ })
+connectdb()
+
